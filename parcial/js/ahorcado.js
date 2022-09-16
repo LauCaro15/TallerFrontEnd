bd482dd383e34791822c6d6d6f251e0adbfc3c2d
Jean Ballesteros <jeanc.ballesterosz@autonoma.edu.co>
2022-09-16 15:19:51 -0500

    avance

--- parcial/js/ahorcado.js
+++ parcial/js/ahorcado.js
@@ -1,1 +1,18 @@
-var palabra =document.getElementById("").value
+function calcular(){
+    var palabra = document.getElementById("palabra").value;
+    alert(palabra.length);
+
+    var arrayLetras = [];
+
+    var contador = 0;
+
+    for (let i = 0; i < palabra.length; i++) {
+        var letra = palabra.charAt(i);
+        arrayLetras.push(letra);
+        // alert(letra);
+
+
+    }
+    console.log(arrayLetras);
+}
+
